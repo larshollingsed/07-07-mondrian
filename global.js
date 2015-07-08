6e8a1d43be43464ef2f56c206111d4b864782a04
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-07 21:27:26 -0500

    Added current brush color box

--- global.js
+++ global.js
@@ -2,15 +2,22 @@ var brush;
 
 function red_button() {
   brush = "red";
+  update_brush_display();
 };
 
 function blue_button() {
   brush = "blue";
+  update_brush_display();
 };
 
 function yellow_button() {
   brush = "yellow";
+  update_brush_display();
 };
+
+function update_brush_display() {
+  document.getElementById("brush_color").style.background = brush;
+}
 
 document.getElementById("red_button").onclick = red_button;
 document.getElementById("blue_button").onclick = blue_button;
@@ -20,5 +27,4 @@ document.getElementById("canvas").onclick = change_color;
 
 function change_color() {
   document.getElementById("canvas").style.background = brush;
-  
 }
